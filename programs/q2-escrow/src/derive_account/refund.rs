4a12eaaac1874df1ec4c4c82dbef5adb8d923765
Nikunj <mnikunj2622@gmail.com>
2026-05-15 19:27:25 +0530

    impl: refuund derive acc

---
+++ programs/q2-escrow/src/derive_account/refund.rs
@@ -0,0 +1,46 @@
+use crate::Escrow;
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+
+#[derive(Accounts)]
+pub struct Refund<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>, 
+
+    #[account(mint::token_program = token_program)]
+    pub mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+    )]
+    pub maker_ata_a: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut, 
+        close = maker,
+        has_one = mint_a,
+        has_one = maker,
+        seeds = [b"escrow", maker.key().as_ref(), mint_a.key().as_ref()],
+        bump = escrow.bump
+    )]
+    pub escrow: Account<'info, Escrow>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority = escrow,
+        associated_token::token_program = token_program
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
